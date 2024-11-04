d4962409862a67693c37b979d421ac886369ef03
Martin-Norton <martinnorton07@gmail.com>
2024-11-04 18:37:48 -0300

    2.5.0

--- index.js
+++ index.js
@@ -17,11 +17,24 @@ const listaEsperaRoutes = require('./src/routes/listaEspera');
 const app = express();
 const PORT = 3000;
 
+const conection  = require('./src/database/connectionMySQL');
+
 app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
+
+const bcrypt = require('bcryptjs'); 
+const session = require('express-session');
+
+app.use(session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+}))
+
 app.use(express.static(path.join(__dirname,'src' ,'public')));
 app.use(methodOverride('_method'));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -37,6 +50,13 @@ app.use('/', turnoRoutes);
 app.use('/', pacienteRoutes);
 app.use('/', clasificacionRoutes);
 app.use('/', listaEsperaRoutes);
+
+app.get('/login/this', (req, res) => {
+    res.render('login');
+});
+app.get('/register/this', (req, res) => {
+    res.render('register');
+});
 
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto http://localhost:${PORT}`);
